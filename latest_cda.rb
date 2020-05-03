07e58816ab87a84f9cbdc2a078b7f95fd6276dd7
志村 <shimura-hir@obun.biz>
2020-05-03 19:43:54 +0900

    take items by ARGV[2]

--- latest_cda.rb
+++ latest_cda.rb
@@ -7,13 +7,15 @@ masses       = cdas.select{ |cda| /Mass/        =~ cda }
 hs = Time.local *(ARGV[1].split(/\D/))
 birthday = "#{hs.strftime '%Y/%m/%d'} (#{Time.at(Time.now - hs).year - 1970})"
 
+t, p, m = ARGV[2].to_s.split /\D/
+
 [
   ARGV[3..-1].to_a.insert(1, birthday).compact.join(', '),
   '',
-  temperatures.take(27),
+  temperatures.take(t ? t.to_i : 27),
   '',
-  pressures.sort.reverse.take(18),
+  pressures.sort.reverse.take(p ? p.to_i : 18),
   '',
-  masses.take(15),
+  masses.take(m ? m.to_i : 15),
 ] \
   .flatten.join("\n").display
