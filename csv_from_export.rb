b46dd2b4ad35c7629e742157268be1919969390c
志村 <shimura-hir@obun.biz>
2020-05-31 21:20:10 +0900

    export.xml to csv, not export_cda.xml

---
+++ csv_from_export.rb
@@ -0,0 +1,19 @@
+require 'rexml/document'
+
+(start = Time.now).to_s.+("\n").display $stderr
+REXML::Document.new(ARGF.read) \
+  .root \
+  .get_elements('//Record') \
+  .map do |record|
+    #record.attributes.inspect
+    #record.attribute('type').value
+    %w[value unit startDate endDate type] \
+    .map do |name|
+      record.attribute(name).value.sub('HKQuantityTypeIdentifier','')
+    end \
+    .join(',')
+  end \
+  .join("\n") \
+  .display
+(stop = Time.now).to_s.+("\n").display $stderr
+(stop-start).to_s.+("\n").display $stderr
