97ff554f12b59f6134b6d1e7811e35da8a497331
志村 <shimura-hir@obun.biz>
2020-06-03 13:36:14 +0900

    steps[Date.parse $F[2]] += $F[0].to_i

--- oga_csv_from_export.rb
+++ oga_csv_from_export.rb
@@ -2,6 +2,9 @@ require 'oga'
 # csv from export: require Oga. 6sec(nano) Ruby 2.4.5
 # <-  900sec(CF-RZ6) REXML Ruby 2.4.4
 # <- 3800sec(CF-S10) REXML Ruby 1.9.3
+#
+# grep Step oga.export.csv | ruby -rtime -aF, -lne 'BEGIN{steps=Hash.new{0}}; steps[Date.parse $F[2]] += $F[0].to_i; END{steps.sort.map{|k,v| "#{k.strftime "%y-%m-%d(%a)"}:#{"%5d"%v}\n" }.join.display}' | less
+
 
 (start = Time.now).to_s.+("\n").display $stderr
 Oga::XML::Parser.new(ARGF.read) \
