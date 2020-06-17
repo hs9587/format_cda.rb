e3232d04e5ada4affdf247539dc24ab60179badd
志村 <shimura-hir@obun.biz>
2020-06-17 12:45:25 +0900

    DailyCounts # add

--- count_csv_from_export.rb
+++ count_csv_from_export.rb
@@ -9,16 +9,20 @@ class DailyCounts < Hash
       hash[key] = Counts.new{0.0}
     end # super() do |hash, key|
   end # def initialize
+
+  def add(row)
+    self[Date.parse row[2].to_s][row[4]] += row[0].to_f
+  end # def add(row)
 end # class DailyCounts < Hash
 
 CSV::Converters[:time23] = ->(cell, info) \
   { ([2,3].include? info.index) ? Time.parse(cell) : cell }
 
-csvs = CSV.parse ARGF.read, converters: :time23
-csvs.size.to_s.+("\n").display
+csv = CSV.parse ARGF.read, converters: :time23
+csv.size.to_s.+("\n").display
 
-dcs = csvs.each_with_object(DailyCounts.new) do |csv, dc|
-  dc[Date.parse csv[2].to_s][csv[4]] += csv[0].to_f
-end # dcs = csvs.each_with_object(DailyCounts.new) do |csv, dc|
+dcs = DailyCounts.new
+csv.each{ |row| dcs.add row }
 
 (dcs.keys.min..dcs.keys.max).each{ |day| "#{day}: #{dcs[day]}\n".display }
+
