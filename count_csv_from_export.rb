59defadd47e90e74d502da69ed678f034439e329
志村 <shimura-hir@obun.biz>
2020-06-20 23:09:13 +0900

    case row[0] for row[1,2,3]

--- count_csv_from_export.rb
+++ count_csv_from_export.rb
@@ -3,27 +3,11 @@ require 'time'
 
 class Counts < Hash
   def add(row)
-    case key = row[4]
-    when 'BloodPressureSystolic', 'BloodPressureDiastolic'
-    when 'BodyMass', 'BodyTemperature' then
-      self[key] = self[key] ? self[key] << row : [row]
-    when 'StepCount', 'FlightsClimbed' then # sum of integer
-      self[key] = if self[key] then
-        [self[key][0] + row[0].to_i, self[key][1]]
-      else# self[key] = if self[key]
-        [row[0].to_i, row[1]] 
-      end # self[key] = if self[key]
-    when 'DistanceWalkingRunning' then # sum of float
-      self[key] = if self[key] then
-        [self[key][0] + row[0].to_f, self[key][1]]
-      else# self[key] = if self[key]
-        [row[0].to_f, row[1]] 
-      end # self[key] = if self[key]
-    when 'HeadphoneAudioExposure' then
-      self[key] = self[key] ? self[key] << row : [row]
+    case key = row[0]
+    when /BloodPressureSystolic/, /BloodPressureDiastolic/
     else
       self[key] = self[key] ? self[key] << row : [row]
-    end # case key = row[4]
+    end # case key = row[0]
   end # def add(row)
 end # class Counts < Hash
 
@@ -35,15 +19,17 @@ class DailyCounts < Hash
   end # def initialize
 
   def add(row)
-    self[Date.parse row[2].to_s].add row
+    self[Date.parse row[1].to_s].add row
   end # def add(row)
 end # class DailyCounts < Hash
 
-# %w[value unit startDate endDate type]
-CSV::Converters[:time23] = ->(cell, info) \
-  { ([2,3].include? info.index) ? Time.parse(cell) : cell }
+# type_dates = %w[type startDate endDate creationDate sourceName sourceVersion]
+# type_dates.+(%w[value unit]) 
+#              %w[type value unit]
+CSV::Converters[:time13] = ->(cell, info) \
+  { ([1,3].include? info.index) ? Time.parse(cell) : cell }
 
-csv = CSV.parse ARGF.read, converters: :time23
+csv = CSV.parse ARGF.read, converters: :time13
 csv.size.to_s.+("\n").display
 
 dcs = DailyCounts.new
