ce8741467c803904bd22fc44f0f361a9114253e1
志村 <shimura-hir@obun.biz>
2020-06-20 19:20:15 +0900

    //Record and //Correlation

--- oga_csv_from_export.rb
+++ oga_csv_from_export.rb
@@ -5,19 +5,55 @@ require 'oga'
 #
 # grep Step oga.export.csv | ruby -rtime -aF, -lne 'BEGIN{steps=Hash.new{0}}; steps[Date.parse $F[2]] += $F[0].to_i; END{steps.sort.map{|k,v| "#{k.strftime "%y-%m-%d(%a)"}:#{"%5d"%v}\n" }.join.display}' | less
 
+type_dates = %w[type startDate endDate creationDate sourceName sourceVersion]
+csv = []
 
 (start = Time.now).to_s.+("\n").display $stderr
 Oga::XML::Parser.new(ARGF.read) \
   .parse \
-  .xpath('//Record') \
-  .map do |record|
-    %w[value unit startDate endDate creationDate type sourceName sourceVersion]\
-    .map do |key|
-      record[key].sub('HKQuantityTypeIdentifier','')
-    end \
-    .join(',')
+  .tap do |xml|
+    xml \
+    .xpath('//Record') \
+    .each do |record|
+      csv << 
+      (
+        type_dates.+(%w[value unit]) \
+        .map do |key|
+          record[key]
+        end \
+        .join(',') \
+        .+("\n")
+      )
+    end # .each do |record|
   end \
-  .join("\n") \
-  .display
+    .xpath('//Correlation') \
+    .each do |cor|
+      csv <<
+      (
+        type_dates \
+        .map do |key|
+          cor[key]
+        end \
+        .join(',') \
+        + ','  \
+        + (
+          cor \
+          .xpath('Record') \
+          .map do |record|
+            %w[type value unit]\
+            .map do |key|
+              record[key].sub('HKQuantityTypeIdentifier','')
+            end \
+            .join(',')
+          end # .map do |record|
+          .join(',')
+        ) \
+        .+("\n")
+      )
+    end # .each do |cor|
+  # end \
+# Oga::XML::Parser.new(ARGF.read) \
 (stop = Time.now).to_s.+("\n").display $stderr
 (stop-start).to_s.+("\n").display $stderr
+
+csv.join.display
