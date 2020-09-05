d19a2daf929b1d4bf68f0f91e8c04336d4c04658
志村 <shimura-hir@obun.biz>
2020-09-05 19:22:00 +0900

    String[]

--- locale/export.ja.rb
+++ locale/export.ja.rb
@@ -4,7 +4,8 @@
     date: {
       formats: {
         default: ->(date, params) \
-          { "%Y-%m-%d(#{%w[日 月 火 水 木 金 土 日][date.wday]})" }
+          { "%Y-%m-%d(#{'日月火水木金土日'[date.wday]})" }
+          #{ "%Y-%m-%d(#{%w[日 月 火 水 木 金 土 日][date.wday]})" }
       }
     }
   }
