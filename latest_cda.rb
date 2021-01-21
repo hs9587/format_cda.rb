f74e0b5f48b7946aa1d5d05322a724ac52b9fcc2
志村 <shimura-hir@obun.biz>
2021-01-22 07:49:10 +0900

    Oxygens

--- latest_cda.rb
+++ latest_cda.rb
@@ -3,17 +3,23 @@ cdas = File.read(ARGV[0]).split "\n"
 temperatures = cdas.select{ |cda| /Temperature/ =~ cda }
 pressures    = cdas.select{ |cda| /Pressure/    =~ cda }
 masses       = cdas.select{ |cda| /Mass/        =~ cda }
+oxygen_s     = cdas.select{ |cda| /Oxygen/      =~ cda }
 
 hs = Time.local *(ARGV[1].split /\D/)
 birthday = "#{hs.strftime '%Y/%m/%d'} (#{Time.at(Time.now - hs).year - 1970})"
 
 t, p, m = ARGV[2].to_s.split /\D/
+#t, o, p, m = ARGV[2].to_s.split /\D/
 
 [
   ARGV[3..-1].to_a.insert(1, birthday).compact.join(', '),
     # .to_a .compact は ARGV[3 以降] が無かった時対策
   '',
   temperatures.take(t ? t.to_i : 25),
+#  '',
+#  oxygen_s.take(o ? o.to_i : 25),
+#  '',
+#  ARGV[3..-1].to_a.insert(1, birthday).compact.join(', '),
   '',
   pressures.sort.reverse.take(p ? p.to_i : 16),
   '',
