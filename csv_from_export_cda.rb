837769c4db42ba3987c461850b52d55a151ea13d
志村 <shimura-hir@obun.biz>
2021-01-23 19:08:32 +0900

    # 単位が % で値が 1に満たないときパーセント表記用に 100倍する

--- csv_from_export_cda.rb
+++ csv_from_export_cda.rb
@@ -14,7 +14,11 @@ REXML::Document.new(ARGF.read) \
     .map do |key, (method, value)|
       comp.get_elements("*//#{key}").first.send(method).send(value)
     end \
-      .tap{|row| unit = row[2] }.map.with_index{|cell, i| (i==1 and unit == '%' and cell.to_f<1) ? cell.to_f.*(100).to_s : cell} \
+      .tap{|row| unit = row[2] } \
+      .map.with_index do |cell, i|
+        (i==1 and unit == '%' and cell.to_f<1) ? cell.to_f.*(100).to_s : cell
+        # 単位が % で値が 1に満たないときパーセント表記用に 100倍する
+      end \
     .join(',') \
     .+("\n")
   end \
