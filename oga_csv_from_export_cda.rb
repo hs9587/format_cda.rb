070560feb26c48004d8fbe2f6ef2bde2b3c410c9
志村 <shimura-hir@obun.biz>
2021-10-13 08:59:14 +0900

    # 単位が % で値が 1に満たないときパーセント表記用に 100倍する

--- oga_csv_from_export_cda.rb
+++ oga_csv_from_export_cda.rb
@@ -1,4 +1,5 @@
 require 'oga'
+unit = nil
 
 Oga::XML::Parser.new(ARGF.read) \
   .parse \
@@ -12,6 +13,11 @@ Oga::XML::Parser.new(ARGF.read) \
     }.map do |key, method|
       comp.xpath(".//#{key}").send(*method)
     end \
+      .tap{|row| unit = row[2] } \
+      .map.with_index do |cell, i|
+        (i==1 and unit == '%' and cell.to_f<1) ? cell.to_f.*(100).to_s : cell
+        # 単位が % で値が 1に満たないときパーセント表記用に 100倍する
+      end \
     .join(',') \
     .+("\n")
   end \
